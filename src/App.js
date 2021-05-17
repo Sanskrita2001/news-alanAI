d525091bc36a9a4f05f39c1e421e71705dfd3faa
Sanskrita Saha <sanskrita2001@gmail.com>
2021-05-17 14:01:58 +0530

    UI Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import { useEffect } from "react";
+import { useState, useEffect } from 'react';
 import './App.css';
-import alanBtn from "@alan-ai/alan-sdk-web";
+import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './components/NewsCards/NewsCards';
 
 const alanKey =
 	'06a4eb1dc95cb6cb0105e6a7f21a4fbf2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 function App() {
-  useEffect(() => {
-    alanBtn({
-      key: alanKey,
-      onCommand: ({command,articles}) => {
-        if (command === 'newsHeadlines') {
-          console.log(articles);
+	const [newsArticle, setNewsArticle] = useState([]);
+	useEffect(() => {
+		alanBtn({
+			key: alanKey,
+			onCommand: ({ command, articles }) => {
+				if (command === 'newsHeadlines') {
+					console.log(articles);
+					setNewsArticle(articles);
 				}
-      }
-    })
-  }, [])
-  return (
-    <div >
-      Henlo
-    </div>
-  );
+			},
+		});
+	}, []);
+	return (
+		<div>
+			<h1>ALAN AI News App</h1>
+			<NewsCards articles={newsArticle} />
+		</div>
+	);
 }
 
 export default App;
